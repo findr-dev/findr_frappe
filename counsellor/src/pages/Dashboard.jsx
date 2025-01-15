c5ebb231734fb796b5aa9c96f01fb4129658ed45
G0KS <goksrofficial@gmail.com>
2025-01-15 15:40:11 +0530

    added auth for team

--- counsellor/src/pages/Dashboard.jsx
+++ counsellor/src/pages/Dashboard.jsx
@@ -1,96 +1,105 @@
 import Card from "../components/Card";
-import { useFrappeGetDocCount } from "frappe-react-sdk";
-import {  useState } from "react";
+import { useFrappeAuth, useFrappeGetDocCount } from "frappe-react-sdk";
+import { useState } from "react";
+import { AuthComponent } from "../auth/Auth";
 
 function Dashboard() {
-   const [newlyPaid, setNewlyPaid] = useState({});
-   const [existingStudent, setExistingStudent] = useState({});
-   const [isLoading, setIsLoading] = useState(true);
+  const [newlyPaid, setNewlyPaid] = useState({});
+  const [existingStudent, setExistingStudent] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
-   const cards = [
-      {
-         id: "1",
-         image: (
-            <span className="material-symbols-outlined text-green-800 text-9xl text-center flex h-full justify-center align-middle">
-               group_add
-            </span>
-         ),
-         title: "Students List",
-         description: `We have ${newlyPaid} new students`,
-         location: "students/new",
-      },
-      {
-         id: "2",
-         image: (
-            <span className="material-symbols-outlined text-green-800 text-9xl text-center flex h-full justify-center align-middle">
-               partner_exchange
-            </span>
-         ),
-         title: "Attended Students",
-         description: `You have given courses to ${existingStudent} students`,
-         location: "students",
-      },
-   ];
+  const { currentUser } = useFrappeAuth();
 
-   return (
-      <div className="container lg:px-24 px-4 py-24 h-dvh">
-         <GetNewStudentsCount
+  const cards = [
+    {
+      id: "1",
+      image: (
+        <span className="material-symbols-outlined text-green-800 text-9xl text-center flex h-full justify-center align-middle">
+          group_add
+        </span>
+      ),
+      title: "Students List",
+      description: `We have ${newlyPaid} new students`,
+      location: "students/new",
+    },
+    {
+      id: "2",
+      image: (
+        <span className="material-symbols-outlined text-green-800 text-9xl text-center flex h-full justify-center align-middle">
+          partner_exchange
+        </span>
+      ),
+      title: "Attended Students",
+      description: `You have given courses to ${existingStudent} students`,
+      location: "students",
+    },
+  ];
+
+  return (
+    <div className="container lg:px-24 px-4 py-24 h-dvh">
+      {currentUser ? (
+        <>
+          <GetNewStudentsCount
             setNewlyPaid={setNewlyPaid}
             setIsLoading={setIsLoading}
-         />
-         <GetExisitngStudentsCount
+          />
+          <GetExisitngStudentsCount
             setExistingStudent={setExistingStudent}
             setIsLoading={setIsLoading}
-         />
-         <div className="title">
+          />
+          <div className="title">
             <p className="text-4xl lg:text-5xl  text-[#0f6990]">
-               Welcome back Counsellor
+              Welcome back Counsellor
             </p>
-         </div>
-         {isLoading ? (
+          </div>
+          {isLoading ? (
             <div className="h-dvh flex justify-center align-middle">
-               <div className="loader"></div>
+              <div className="loader"></div>
             </div>
-         ) : (
+          ) : (
             <div className="cardLayout flex flex-wrap justify-evenly align-middle gap-8">
-               {/* students list */}
-               {cards.map((card) => (
-                  <Card key={card.id} card={card} />
-               ))}
+              {/* students list */}
+              {cards.map((card) => (
+                <Card key={card.id} card={card} />
+              ))}
             </div>
-         )}
-      </div>
-   );
+          )}
+        </>
+      ) : (
+        <AuthComponent />
+      )}
+    </div>
+  );
 }
 
 export default Dashboard;
 
 const GetNewStudentsCount = ({ setNewlyPaid, setIsLoading }) => {
-   const { data, isLoading } = useFrappeGetDocCount("Student", [
-      ["registration_fee", "=", "1"],
-      ["course_added", "=", "0"],
-   ]);
+  const { data, isLoading } = useFrappeGetDocCount("Student", [
+    ["registration_fee", "=", "1"],
+    ["course_added", "=", "0"],
+  ]);
 
-   if (isLoading) setIsLoading(isLoading);
-   else {
-      setIsLoading(isLoading);
-      setNewlyPaid(data);
-   }
+  if (isLoading) setIsLoading(isLoading);
+  else {
+    setIsLoading(isLoading);
+    setNewlyPaid(data);
+  }
 
-   return null;
+  return null;
 };
 
 const GetExisitngStudentsCount = ({ setExistingStudent, setIsLoading }) => {
-   const { data, isLoading } = useFrappeGetDocCount("Student", [
-      ["registration_fee", "=", "1"],
-      ["course_added", "=", "1"],
-   ]);
+  const { data, isLoading } = useFrappeGetDocCount("Student", [
+    ["registration_fee", "=", "1"],
+    ["course_added", "=", "1"],
+  ]);
 
-   if (isLoading) setIsLoading(isLoading);
-   else {
-      setIsLoading(isLoading);
-      setExistingStudent(data);
-   }
+  if (isLoading) setIsLoading(isLoading);
+  else {
+    setIsLoading(isLoading);
+    setExistingStudent(data);
+  }
 
-   return null;
+  return null;
 };

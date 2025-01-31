cbc54e2e251be6f176ea756fb707dc4c2c0c1fdd
findr-dev <findrdeveloper@gmail.com>
2025-01-31 15:57:30 +0000

    next and previous button added to studentspage

--- counsellor/src/pages/StudentsPage.jsx
+++ counsellor/src/pages/StudentsPage.jsx
@@ -41,49 +41,51 @@ function StudentsPage() {
           <div className="loader"></div>
         </div>
       ) : (
-        <div className="cardLayout mt-8 flex flex-wrap justify-center align-middle gap-6">
-          {data?.map((student) => (
-            <div
-              className="group card w-full lg:w-1/4 rounded-2xl shadow-2xl p-7 cursor-pointer bg-white hover:bg-[#0f6990] transition ease-in-out duration-300"
-              key={student.idx}
-              onClick={() => navigate(`/students/${student.name}`)}
-            >
-              <div className="flex gap-2">
-                <span className="material-symbols-outlined text-[#0f6990] group-hover:text-white">
-                  face
-                </span>
-                <div>
-                  <p className="text-sm text-[#0f6990] group-hover:text-white">
-                    {student.name}
-                  </p>
-                  <h3 className="group-hover:text-white">
-                    {student.first_name} {student.last_name}
-                  </h3>
-                  <p className="text-sm text-gray-600 group-hover:text-white">
-                    Looking for {student.education_program}
-                  </p>
+        <>
+          <div className="cardLayout mt-8 flex flex-wrap justify-center align-middle gap-6">
+            {data?.map((student) => (
+              <div
+                className="group card w-full lg:w-1/4 rounded-2xl shadow-2xl p-7 cursor-pointer bg-white hover:bg-[#0f6990] transition ease-in-out duration-300"
+                key={student.idx}
+                onClick={() => navigate(`/students/${student.name}`)}
+              >
+                <div className="flex gap-2">
+                  <span className="material-symbols-outlined text-[#0f6990] group-hover:text-white">
+                    face
+                  </span>
+                  <div>
+                    <p className="text-sm text-[#0f6990] group-hover:text-white">
+                      {student.name}
+                    </p>
+                    <h3 className="group-hover:text-white">
+                      {student.first_name} {student.last_name}
+                    </h3>
+                    <p className="text-sm text-gray-600 group-hover:text-white">
+                      Looking for {student.education_program}
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-      )}
+            ))}
+          </div>
           <div className="mt-8 flex flex-wrap justify-center align-middle gap-6">
             {pageIndex > 0 && (
               <button
                 onClick={() => setPageIndex(pageIndex - 12)}
-                className="text-white mt-2 px-5 py-1 shadow-lg rounded-xl bg-[#0f6990] hover:translate-x hover:scale-105 transition ease-in-out duration-300"
+                className="text-white mt-2 px-5 py-2 shadow-lg rounded-xl bg-[#0f6990] hover:translate-x hover:scale-105 transition ease-in-out duration-300"
               >
                 Previous
               </button>
             )}
             <button
               onClick={() => setPageIndex(pageIndex + 12)}
-              className="text-white mt-2 px-5 py-1 shadow-lg rounded-xl bg-green-600 hover:translate-x hover:scale-105 transition ease-in-out duration-300"
+              className="text-white mt-2 px-5 py-2 shadow-lg rounded-xl bg-green-600 hover:translate-x hover:scale-105 transition ease-in-out duration-300"
             >
               Next
             </button>
           </div>
-        </div>
+        </>
+      )}
     </div>
   );
 }
